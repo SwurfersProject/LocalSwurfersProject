43bd404b075a8097cc2d92a8fddfb85a26ed25b4
Swurfers Project <swurfersproject@gmail.com>
2016-03-06 01:24:49 +0000

    Adicionado ao Repositorio

---
+++ Unity/SwurfersProject/Assets/Scenes/Movement (1).js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var forwardRate : float =3;
+var turnRate : float = 2;
+
+
+function Start () {
+}
+
+function Update () 
+{
+	var forwardMoveAmount = Input.GetAxis("Vertical") * forwardRate;
+
+	var turnForce = Input.GetAxis("Horizontal") * turnRate;
+
+	transform.Rotate(0, turnForce, 0);
+
+	transform.position += transform.forward * forwardMoveAmount * Time.deltaTime;
+
+}
---
+++ Unity/SwurfersProject/Assets/Scripts/Player.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+
+    public float speed = .0f;
+
+    public float smooth = 2.0F;
+    public float tiltAngle = 30.0F;
+    private Quaternion initialRotation;
+    private Quaternion gyroInitialRotation;
+    public Rigidbody rb;
+
+    public float yRotation = 5.0F;
+    private float xRotation;
+
+    void Start()
+    {
+        initialRotation = transform.rotation;
+        gyroInitialRotation = Input.gyro.attitude;
+
+    }
+
+    void Update()
+    {
+
+        int nbTouches = Input.touchCount;
+
+        if (nbTouches > 0)
+        {
+            for (int i = 0; i < nbTouches; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+
+                        break;
+                    case TouchPhase.Moved:
+                        break;
+                    case TouchPhase.Stationary:
+                        if (touch.tapCount == 1)
+                        {
+                          AccelerateForward(transform);
+                        }
+                        break;
+                    case TouchPhase.Ended:
+                        StopMoving(transform);
+                        break;
+                    case TouchPhase.Canceled:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+        }
+        else
+        {
+            StopMoving(transform);
+        }
+
+
+
+
+       // MoveSides();
+        
+
+       
+
+    }
+
+ 
+
+    private void MoveSides()
+    {
+        Quaternion offsetRotation = Quaternion.Inverse(gyroInitialRotation) * Input.gyro.attitude;
+        transform.localRotation = initialRotation * offsetRotation;
+        
+        //transform.Rotate(Vector3.right * Time.deltaTime);
+        transform.Rotate(Vector3.up * Time.deltaTime, transform.localPosition.z);
+    }
+
+    private void AccelerateForward(Transform myMove)
+    {
+       myMove.Translate(Input.acceleration.x, 0, -Input.acceleration.z);
+
+
+        
+    }
+
+    private void StopMoving(Transform myMove)
+    {
+        myMove.Translate(0, 0, 0);
+    }
+
+    void OnGUI()
+    {
+
+    }
+}
